af1da4e9cae2b6143fe97b2116cf53387db6c5ff
MuhHanif <48muhhanif@gmail.com>
2023-02-06 14:05:01 +0700

    add memory efficient params

--- examples/text_to_image/train_text_to_image_flax.py
+++ examples/text_to_image/train_text_to_image_flax.py
@@ -394,7 +394,7 @@ def main():
         args.pretrained_model_name_or_path, subfolder="vae", dtype=weight_dtype
     )
     unet, unet_params = FlaxUNet2DConditionModel.from_pretrained(
-        args.pretrained_model_name_or_path, subfolder="unet", dtype=weight_dtype
+        args.pretrained_model_name_or_path, subfolder="unet", dtype=weight_dtype, use_memory_efficient=True
     )
 
     # Optimization
