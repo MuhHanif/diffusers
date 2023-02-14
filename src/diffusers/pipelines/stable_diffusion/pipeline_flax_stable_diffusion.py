b9e07bfd456b3c135c9819c24e8b82d08fb215a1
MuhHanif <48muhhanif@gmail.com>
2023-02-14 17:23:11 +0700

    edit the params outside the call :(

--- src/diffusers/pipelines/stable_diffusion/pipeline_flax_stable_diffusion.py
+++ src/diffusers/pipelines/stable_diffusion/pipeline_flax_stable_diffusion.py
@@ -350,38 +350,7 @@ class FlaxStableDiffusionPipeline(FlaxDiffusionPipeline):
         self.text_encoder.config.num_hidden_layers = layer_count 
 
         if interpolate_position_embeddings > 1:
-            #reference positional embedding
-            positional_embedding = self.text_encoder.params["text_model"]["embeddings"]["position_embedding"]["embedding"]
-
-            #create interpolation map without first and last value
-            positional_embedding_linear = np.arange(positional_embedding[1:-1].shape[0])
-            new_positional_embedding_length = np.arange(
-                0, 
-                positional_embedding[1:-1].shape[0] + 1/interpolate_position_embeddings - 1, 
-                1/interpolate_position_embeddings
-            )
-
-            #interpolate across token length axis
-            interpolated_positional_embeddings = interp1d(
-                positional_embedding_linear, 
-                positional_embedding[1:-1], 
-                axis=0
-            )
-            interpolated_value = interpolated_positional_embeddings(new_positional_embedding_length)
-
-            #concatenate first and last position back
-            positional_embedding_first = np.expand_dims(positional_embedding[0], axis=0)
-            positional_embedding_last = np.expand_dims(positional_embedding[-1], axis=0)
-            capped_interpolated_value = np.concatenate([
-                positional_embedding_first, 
-                interpolated_value, 
-                positional_embedding_last
-                ])
-            
-            #assign new value back to text encoder params
-            text_encoder.params["text_model"]["embeddings"]["position_embedding"]["embedding"] = capped_interpolated_value
-
-            text_encoder.config.max_position_embeddings = capped_interpolated_value.shape[0]
+            text_encoder.config.max_position_embeddings = text_encoder.params["text_model"]["embeddings"]["position_embedding"]["embedding"].shape[0]
 
         if isinstance(guidance_scale, float):
             # Convert to a tensor so each device gets a copy. Follow the prompt_ids for
